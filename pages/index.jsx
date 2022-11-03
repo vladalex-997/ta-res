d5f44a151eae2159679ab0753379c98fbbe48970
Vlad Alex <vladalex997@gmail.com>
2022-11-03 22:02:03 +0200

    curs 3/11

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,17 @@ export default function Home() {
   <div>
   Hello  <i className="fa-solid fa-house"></i>
     <img src="/images/logo.jpg" alt="Alt" />
+
+<button className="button button-primary">Button Primary</button>
+
+<button className="button button-primary button-large">Button primary Large</button>
+
+<a className="button button-primary">Button Primary</a>
+
+<a className="button button-primary button-large">Button primary Large</a>
+
+
+
   </div>
   );
 }
